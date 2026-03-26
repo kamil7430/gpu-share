392c312cee7ae29cf6bcafc3fb19de38f7ae7394
Kamil Błażejczyk <01189072@pw.edu.pl>
2026-03-26 19:11:09 +0100

    Add server file

---
+++ backend/server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"net/http"
+)
+
+func NewServer(db *sql.DB) http.Handler {
+	mux := http.NewServeMux()
+
+	//eventRepo := repository.NewEventRepository(db)
+	//teamRepo := repository.NewTeamRepository(db)
+	//
+	//eventHandler := handler.NewEventHandler(eventRepo)
+	//teamHandler := handler.NewTeamHandler(teamRepo)
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		if err := db.Ping(); err != nil {
+			http.Error(w, "DB not reachable", 500)
+			return
+		}
+		w.Write([]byte("OK"))
+	})
+
+	//mux.HandleFunc("GET /events", eventHandler.GetEvents)
+	//mux.HandleFunc("GET /events/{id}", eventHandler.GetEvent)
+	//mux.HandleFunc("POST /events", eventHandler.CreateEvent)
+	//mux.HandleFunc("GET /teams", teamHandler.GetTeams)
+	//mux.HandleFunc("GET /teams/{id}", teamHandler.GetTeam)
+	//mux.HandleFunc("POST /teams", teamHandler.CreateTeam)
+
+	fs := http.FileServer(http.Dir("/app/frontend"))
+	mux.Handle("/", fs)
+
+	return mux
+}
