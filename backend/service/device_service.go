4575ed9be8899ac572b7eaa4ea27c1de9ecb03cf
Kamil Błażejczyk <01189072@pw.edu.pl>
2026-03-28 11:45:28 +0100

    Begin service

---
+++ backend/service/device_service.go
@@ -0,0 +1,9 @@
+package service
+
+import "github.com/kamil7430/gpu-share/repository"
+
+type DeviceService struct {
+	r repository.DeviceRepository
+}
+
+func (s *DeviceService) GetDeviceStatusById(id int)
