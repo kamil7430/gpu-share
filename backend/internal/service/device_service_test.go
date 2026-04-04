33633e2489a2eb97193121f6a7aa0abb23647258
Kamil Blazejczyk <kamileczus@gmail.com>
2026-04-04 22:24:59 +0200

    Change title of tests

--- backend/internal/service/device_service_test.go
+++ backend/internal/service/device_service_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestDatabaseDeviceRepository(t *testing.T) {
+func TestDeviceService(t *testing.T) {
 	db, err := internal.InitializeDatabaseConnection(false)
 	require.NoError(t, err)
 
