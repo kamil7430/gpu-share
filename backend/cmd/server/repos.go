ec4d6c3405d0110a84933a07377b2ebd2ac9633a
Kamil Błażejczyk <kamil7430@users.noreply.github.com>
2026-04-12 19:17:34 +0200

    Feature/server modularity (#36)
    * Refactor server.go to inject repos
    
    * Remove unused db parameter
    
    * Apply suggestions from FreePlacki

---
+++ backend/cmd/server/repos.go
@@ -0,0 +1,8 @@
+package server
+
+import "github.com/kamil7430/gpu-share/backend/internal/repository"
+
+type Repos struct {
+	DeviceRepo repository.DeviceRepository
+	GpuRepo    repository.GpuRepository
+}
