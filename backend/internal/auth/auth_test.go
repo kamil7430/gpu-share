38c5c9f31881476715164dd0ae429e9e54c1c0a1
Kamil Blazejczyk <kamileczus@gmail.com>
2026-04-13 14:34:49 +0200

    Add 2nd test to auth_test

--- backend/internal/auth/auth_test.go
+++ backend/internal/auth/auth_test.go
@@ -25,13 +25,11 @@ func TestTokens(t *testing.T) {
 		require.Equal(t, user.Admin, token.Admin)
 	})
 
-	//t.Run("modified token -- should fail", func(t *testing.T) {
-	//	tokenBase, err := CreateToken(&user)
-	//	require.NoError(t, err)
-	//	require.NotEmpty(t, tokenBase)
-	//
-	//	token, err := ParseToken(tokenBase)
-	//	require.Error(t, err)
-	//	require.Nil(t, token)
-	//})
+	t.Run("modified token (admin changed to true) -- should fail", func(t *testing.T) {
+		tokenBase := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RVc2VyIiwiYWRtaW4iOnRydWUsImV4cCI6MTc3NjA4MzkzNSwiaWF0IjoxNzc2MDgzMzM1fQ.QwYUtPog_WlB1-ryyyO_zXDEkcv7sR6NUg3JPKvCeCg"
+
+		token, err := ParseToken(tokenBase)
+		require.Error(t, err)
+		require.Nil(t, token)
+	})
 }
