876adf331a6ca11b2d1b8c4dcc79c8038d3750ac
Kamil Blazejczyk <kamileczus@gmail.com>
2026-04-13 14:43:03 +0200

    Implement credentials validator

--- backend/internal/auth/credentials_validator.go
+++ backend/internal/auth/credentials_validator.go
@@ -1,12 +1,35 @@
 package auth
 
+import (
+	"errors"
+	"fmt"
+	"regexp"
+)
+
 func ValidateUsername(username string) error {
-	panic("implement me")
+	const minUsernameLength = 4
+	const maxUsernameLength = 40
+	const allowedCharactersRegex = "^[a-zA-Z0-9_]*$"
+
+	if len(username) < minUsernameLength || len(username) > maxUsernameLength {
+		return fmt.Errorf("username should be between %d and %d characters", minUsernameLength, maxUsernameLength)
+	}
+
+	regex := regexp.MustCompile(allowedCharactersRegex)
+	if !regex.MatchString(username) {
+		return errors.New("username must only contain alphanumeric characters")
+	}
+
+	return nil
 }
 
 func ValidatePassword(password string) error {
 	const minPasswordLength = 8
 	const maxPasswordLength = 18
 
-	panic("implement me")
+	if len(password) < minPasswordLength || len(password) > maxPasswordLength {
+		return fmt.Errorf("password should be between %d and %d characters", minPasswordLength, maxPasswordLength)
+	}
+
+	return nil
 }
