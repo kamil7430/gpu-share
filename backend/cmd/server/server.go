b6ef005ee1a372f2a9d8f4c8ae1e5163d7925603
Paweł Bielecki <contact@pbielecki.com>
2026-05-20 21:54:41 +0200

    Use helper funciton

--- backend/cmd/server/server.go
+++ backend/cmd/server/server.go
@@ -4,11 +4,11 @@ import (
 	"flag"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/kamil7430/gpu-share/backend/internal/api"
 	"github.com/kamil7430/gpu-share/backend/internal/repository"
 	"github.com/kamil7430/gpu-share/backend/internal/service"
+	"github.com/kamil7430/gpu-share/backend/internal/utils"
 )
 
 // Jeden, by wszystkie zgromadzić i w ciemności związać
@@ -33,10 +33,7 @@ func NewServer(store repository.Store) *http.Server {
 		log.Fatal(err)
 	}
 
-	envIp := os.Getenv("BACKEND_IP")
-	if envIp == "" {
-		envIp = "127.0.0.1"
-	}
+	envIp := utils.GetenvOrDefault("BACKEND_IP", "127.0.0.1")
 	ip := flag.String("ip", envIp, "IP of the backend service")
 	port := flag.String("port", "2137", "port of the backend service")
 	flag.Parse()
