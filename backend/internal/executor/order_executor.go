c168344a3ac250f2134f2c2b463ebe36f98f9730
Paweł Bielecki <contact@pbielecki.com>
2026-06-14 23:19:08 +0200

    Finish integration

--- backend/internal/executor/order_executor.go
+++ backend/internal/executor/order_executor.go
@@ -1,17 +1,18 @@
 package executor
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"strings"
 	"time"
 
 	"github.com/kamil7430/gpu-share/backend/internal/api"
 	"github.com/kamil7430/gpu-share/backend/internal/model"
 	"github.com/kamil7430/gpu-share/backend/internal/repository"
+	"github.com/kamil7430/gpu-share/backend/internal/service"
 	"github.com/kamil7430/gpu-share/backend/internal/utils"
 )
 
@@ -53,7 +54,7 @@ func (e *OrderExecutor) processWaitingOrders(ctx context.Context) {
 	for i := range orders {
 		order := &orders[i]
 
-		if err := e.startOrder(ctx, order); err != nil {
+		if err := e.startOrder(ctx, order, e.store); err != nil {
 			log.Printf("executor: failed to start order %d: %v",
 				order.ID, err)
 		}
@@ -63,6 +64,7 @@ func (e *OrderExecutor) processWaitingOrders(ctx context.Context) {
 func (e *OrderExecutor) startOrder(
 	ctx context.Context,
 	order *model.Order,
+	store repository.Store,
 ) error {
 	order.RentalStatus = api.RentalStatusRUNNING
 
@@ -70,23 +72,35 @@ func (e *OrderExecutor) startOrder(
 		return err
 	}
 
-	reqBody := map[string]any{
-		"deviceId": order.DeviceID,
-		"resources": map[string]any{
-			"vRamMb":    1000,
-			"cudaCores": 4000,
+	log.Printf("executing order id %v on device id %v\n", order.ID, order.DeviceID)
+
+	reqBody := strings.NewReader(fmt.Sprintf(`{
+		"deviceId": "%v",
+		"resources": {
+			"vRamMb": 1000,
+			"cudaCores": 4000
 		},
-		"maxTimeSec": 3600,
+		"maxTimeSec": 360
+	}`, order.DeviceID))
+
+	sr := service.NewUserService(store)
+	res, err := sr.Refresh(context.WithValue(ctx, utils.ContextUsernameKey{}, "user9"))
+	if err != nil {
+		return err
 	}
+	token := res.(*api.AuthToken).Token
+	log.Println(token)
 
-	body, _ := json.Marshal(reqBody)
+	coordIp := utils.GetenvOrDefault("GPU_IP", "127.0.0.1")
+	coordPort := "2138"
+	req, err := http.NewRequestWithContext(ctx, "POST", "http://"+coordIp+":"+coordPort+"/api/jobs", reqBody)
 
-	envIp := utils.GetenvOrDefault("COORDINATOR_IP", "127.0.0.1")
-	resp, err := http.Post(
-		"http://"+envIp+"/jobs",
-		"application/json",
-		bytes.NewReader(body),
-	)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "test")
+	dump, _ := httputil.DumpRequestOut(req, true)
+	log.Printf("%s", dump)
+	resp, err := http.DefaultClient.Do(req)
+
 	if err != nil {
 		order.RentalStatus = api.RentalStatusFAILURE
 		_ = e.store.Orders().UpdateOrder(ctx, order)
